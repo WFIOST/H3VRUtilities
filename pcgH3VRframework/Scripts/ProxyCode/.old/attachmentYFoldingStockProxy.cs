be19937ba1babb2746d93cf35cd59a3769b6c1d1
Frityet <ambhogal01@gmail.com>
2020-11-11 10:32:14 -0800

    More Stuff

---
+++ pcgH3VRframework/Scripts/ProxyCode/.old/attachmentYFoldingStockProxy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FistVR;
+using System;
+
+namespace pcgH3VRframework
+{
+	public class attachmentYFoldingStockProxy : FVRInteractiveObject
+	{
+
+		public Transform Root;
+		public Transform Stock;
+		public float MinRot;
+		public float MaxRot;
+		public FVRFoldingStockYAxis.StockPos m_curPos;
+		public FVRFoldingStockYAxis.StockPos m_lastPos;
+		public bool isMinClosed = true;
+		public FVRFireArm FireArm;
+		public FVRFireArmAttachment attachment;
+		public enum StockPos
+		{
+			Closed,
+			Mid,
+			Open
+		}
+
+	}
+}
---
+++ pcgH3VRframework/Scripts/ProxyCode/.old/attachmentYFoldingStockProxyLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using FistVR;
+
+namespace pcgH3VRframework.Scripts.ProxyCode.ProxyLoader
+{
+	public class attachmentYFoldingStockProxyLoader : MonoBehaviour
+	{
+		void Start()
+		{
+			Console.WriteLine("HELLO WORLD! attachmentYFoldingStockProxyLoader doing it's job.");
+			var transferfrom = gameObject.GetComponent<attachmentYFoldingStockProxy>();
+			var transferto = gameObject.AddComponent<attachmentYFoldingStock>();
+
+			//attachmentYFoldingStockProxy vars
+			transferfrom.Root = transferto.Root;
+			transferfrom.Stock = transferto.Stock;
+			transferfrom.MinRot = transferto.MinRot;
+			transferfrom.MaxRot = transferto.MaxRot;
+			transferfrom.m_curPos = transferto.m_curPos;
+			transferfrom.m_lastPos = transferto.m_lastPos;
+			transferfrom.isMinClosed = transferto.isMinClosed;
+			transferfrom.FireArm = transferto.FireArm;
+			transferfrom.attachment = transferto.attachment;
+
+			//FVRInteractiveObject vars
+			transferfrom.ControlType = transferto.ControlType;
+			transferfrom.IsSimpleInteract = transferto.IsSimpleInteract;
+			transferfrom.HandlingGrabSound = transferto.HandlingGrabSound;
+			transferfrom.HandlingReleaseSound = transferto.HandlingReleaseSound;
+			transferfrom.PoseOverride = transferto.PoseOverride;
+			transferfrom.QBPoseOverride = transferto.QBPoseOverride;
+			transferfrom.PoseOverride_Touch = transferto.PoseOverride_Touch;
+			transferfrom.UseGrabPointChild = transferto.UseGrabPointChild;
+			transferfrom.UseGripRotInterp = transferto.UseGripRotInterp;
+			transferfrom.PositionInterpSpeed = transferto.PositionInterpSpeed;
+			transferfrom.RotationInterpSpeed = transferto.RotationInterpSpeed;
+			transferfrom.EndInteractionIfDistant = transferto.EndInteractionIfDistant;
+			transferfrom.EndInteractionDistance = transferto.EndInteractionDistance;
+			transferfrom.m_hand = transferto.m_hand;
+			transferfrom.UXGeo_Hover = transferto.UXGeo_Hover;
+			transferfrom.UXGeo_Held = transferto.UXGeo_Held;
+			transferfrom.UseFilteredHandTransform = transferto.UseFilteredHandTransform;
+			transferfrom.UseFilteredHandPosition = transferto.UseFilteredHandPosition;
+			transferfrom.UseFilteredHandRotation = transferto.UseFilteredHandRotation;
+			transferfrom.UseSecondStepRotationFiltering = transferto.UseSecondStepRotationFiltering;
+			Console.WriteLine("attachmentYFoldingStockProxy done!");
+		}
+	}
+}
+}
