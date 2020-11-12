2c5e3139ec7dbb566af1a8c5b423644d484166bb
potatoes1286 <48143760+gommodore@users.noreply.github.com>
2020-11-12 16:40:33 -0500

    Fixed the AssemblyInfo.
    When loaded, it will now properly display "H3VR Utilities" rather than "pcgH3VRframework". also fixed version, copyright, etc.

--- pcgH3VRframework/Properties/AssemblyInfo.cs
+++ pcgH3VRframework/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("pcgH3VRframework")]
+[assembly: AssemblyTitle("H3VR Utilities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("pcgH3VRframework.mm")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("H3VRUtils.mm")]
+[assembly: AssemblyCopyright("copyright is cringe")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
