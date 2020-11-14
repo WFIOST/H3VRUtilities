b132178b87b89c11d97fc17b3121f6ae15a7206c
potatoes1286 <48143760+gommodore@users.noreply.github.com>
2020-11-14 18:02:39 -0500

    Added the SLN of the stubbed code in there

---
+++ pcgH3VRframework/FVRInteractiveObjects/FVRFirearms/jungleMagCode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace H3VRUtilities.FVRInteractiveObjects.FVRFirearms
+{
+	class jungleMagCode
+	{
+	}
+}
