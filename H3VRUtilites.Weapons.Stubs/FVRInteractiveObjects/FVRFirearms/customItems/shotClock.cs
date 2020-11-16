6673b416cc75cea5f293c9fc529eb5bb71b80d07
potatoes1286 <48143760+gommodore@users.noreply.github.com>
2020-11-16 13:26:32 -0500

    shotclock code added

---
+++ H3VRUtilites.Weapons.Stubs/FVRInteractiveObjects/FVRFirearms/customItems/shotClock.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+using FistVR;
+
+namespace H3VRUtilities.customItems.shotClock
+{
+	class shotClock : MonoBehaviour
+	{
+		public Vector2 startingTimeWindow = new Vector2(2,5);
+		public float stopclock;
+		public int shotsfired;
+
+		public int minTime = 2;
+		public int maxTime = 5;
+
+		public GameObject startbutton;
+		public GameObject stopbutton;
+
+		public bool isClockOn;
+		public bool isInDelayProcess;
+
+		public Text stopclocktext;
+		public Text lastshottext;
+		public Text shotsfiredtext;
+		public Text delaymintext;
+		public Text delaymaxtext;
+
+		private float waittime;
+
+		void New()
+		{
+			
+		}
+
+		void Update()
+		{
+			if (isClockOn)
+			{
+				stopclock += Time.deltaTime;
+				var ts = TimeSpan.FromSeconds(stopclock);
+				stopclocktext.text = string.Format("{0:00}:{0:00}:{0:00}", ts.TotalMinutes, ts.Seconds, ts.Milliseconds);
+			}
+			if (isInDelayProcess)
+			{
+				stopclock += Time.deltaTime;
+				if (stopclock > waittime)
+				{
+					isInDelayProcess = false;
+					StartClock();
+				}
+			}
+			if (isClockOn || isInDelayProcess)
+			{
+				startbutton.SetActive(false);
+				stopbutton.SetActive(true);
+			}
+			else
+			{
+				startbutton.SetActive(true);
+				stopbutton.SetActive(false);
+			}
+		}
+
+		public void startClockProcess()
+		{
+			waittime = UnityEngine.Random.Range((float)minTime, (float)maxTime);
+			isInDelayProcess = true;
+		}
+
+		public void StartClock()
+		{
+			isClockOn = true;
+			stopclock = 0;
+		}
+
+		public void StopClock()
+		{
+			isClockOn = false;
+		}
+
+		public void ShotDetected()
+		{
+			if (isClockOn)
+			{
+				var ts = TimeSpan.FromSeconds(stopclock);
+				lastshottext.text = string.Format("{0:00}:{0:00}:{0:00}", ts.TotalMinutes, ts.Seconds, ts.Milliseconds);
+			}
+		}
+	}
+}
---
+++ H3VRUtilites.Weapons.Stubs/FVRInteractiveObjects/FVRFirearms/customItems/shotClockButton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FistVR;
+using UnityEngine;
+
+namespace H3VRUtilities.customItems.shotClock
+{
+	class shotClockButton : FVRPhysicalObject
+	{
+		public shotClock shotclock;
+
+		public bool pressed;
+		public enum buttonType
+		{
+			start,
+			stop,
+			register,
+			delay
+		}
+		public buttonType button;
+
+		public void Update()
+		{
+			if (pressed){
+				pressed = false;
+				SimpleInteraction(null);
+			}
+		}
+		public override void SimpleInteraction(FVRViveHand hand)
+		{
+			switch (button)
+			{
+				case buttonType.start:
+					shotclock.startClockProcess();
+					break;
+				case buttonType.stop:
+					shotclock.StopClock();
+					break;
+				case buttonType.register:
+
+					break;
+				case buttonType.delay:
+
+					break;
+			}
+		}
+	}
+}
