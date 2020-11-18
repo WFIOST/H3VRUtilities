8fe543a0145fd566aa4cae6e7d94a2284c17f771
Frityet <ambhogal01@gmail.com>
2020-11-17 20:20:33 -0800

    mre

---
+++ H3VRUtilites.Mapping/SosigSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using FistVR;
+using UnityEngine;
+
+namespace H3VRUtils.Mapping
+{
+    public class SosigSpawn : MonoBehaviour
+    {
+        private Transform sosigSpawnLocation;
+        [Header("Choose Sosig Type")]
+        public SosigEnemyID listOfSosigs;
+        
+        
+        
+        private void Awake()
+        {
+            sosigSpawnLocation = this.transform;
+            
+        }
+    }
+}
