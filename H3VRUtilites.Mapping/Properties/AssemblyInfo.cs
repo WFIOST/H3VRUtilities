bd881867acf67a9886c593cba6dda1a3421e8291
potatoes1286 <48143760+gommodore@users.noreply.github.com>
2020-11-19 22:03:22 -0500

    changed dll to .mm.dll

--- H3VRUtilites.Mapping/Properties/AssemblyInfo.cs
+++ H3VRUtilites.Mapping/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("H3VRUtilites.Mapping")]
+[assembly: AssemblyTitle("H3VRUtilites.Mapping.mm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("H3VRUtilites.Mapping")]
+[assembly: AssemblyProduct("H3VRUtilites.Mapping.mm")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
