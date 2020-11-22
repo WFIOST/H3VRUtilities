90f2af959958a5bc635032a843c135f41a4a580c
potatoes1286 <48143760+gommodore@users.noreply.github.com>
2020-11-22 14:56:44 -0500

    fixed meatyceiver2, added shid

--- H3VRUtils.Meatyceiver/Meatyceiver2.cs
+++ H3VRUtils.Meatyceiver/Meatyceiver2.cs
@@ -11,16 +11,21 @@ using BepInEx.Configuration;
 namespace Meatyceiver2
 {
 	[BepInPlugin("dll.potatoes.meatyceiver2", "Meatyceiver2", "0.2")]
-	class Meatyceiver2 : BaseUnityPlugin
+	public class Meatyceiver : BaseUnityPlugin
 	{
-
-		private static ConfigEntry<bool> enableAmmunitionFailures;
 		private static ConfigEntry<bool> enableFirearmFailures;
+		private static ConfigEntry<bool> enableAmmunitionFailures;
 		private static ConfigEntry<bool> enableBrokenFirearmFailures;
+		private static ConfigEntry<bool> enableSecondaryMultipliers;
 
 		private static ConfigEntry<bool> enableConsoleDebugging;
 
 		private static ConfigEntry<float> generalMult;
+		private static ConfigEntry<float> pistolMult;
+
+		private static ConfigEntry<float> failureIncPerRound;
+		private static ConfigEntry<int> minRoundCount;
+
 
 		private static ConfigEntry<float> lightPrimerStrikeFailureRate;
 		private static ConfigEntry<float> HangFireRate;
@@ -34,19 +39,25 @@ namespace Meatyceiver2
 		private static ConfigEntry<float> failureToLockSlide;
 		private static ConfigEntry<float> SlamfireRate;
 
-
+		public static float prevSlideZLock = -999f;
 
 		public static System.Random rnd;
 
-		void Awaken()
+		void Awake()
 		{
+			UnityEngine.Debug.Log("Meatyceiver2 here!");
 			enableAmmunitionFailures = Config.Bind("_General Settings", "Enable Ammunition Failures", true, "Enables ammunition related failures.");
 			enableFirearmFailures = Config.Bind("_General Settings", "Enable Firearm Failures", true, "Enables firearm related failures.");
-			enableBrokenFirearmFailures = Config.Bind("_General Settings", "Enable Broken Firearm Failures", false, "Enables broken firearm related failures.");
+			enableBrokenFirearmFailures = Config.Bind("_General Settings", "Enable Broken Firearm Failures", false, "Enables failures related to permanent firearm damage.");
+			enableSecondaryMultipliers = Config.Bind("_General Settings", "Enable Secondary Failure Multipliers", true, "Enables secondary jam chance multipliers.");
 			enableConsoleDebugging = Config.Bind("_General Settings", "Enable Console Debugging", false, "Exports values and failures to console.");
-			
-			generalMult = Config.Bind("_General Settings", "Failure Chance Multiplier", 1f, "default at 1x is 1%, so this is a more 'pick failure percentage chance'.");
-			
+
+			generalMult = Config.Bind("_Multipliers", "Failure Chance Multiplier", 1f, "default at 1x is 1%, so this is a more 'pick failure percentage chance'.");
+			failureIncPerRound = Config.Bind("_Multipliers", "Additional Failure Chance Per Round", 0.01f, "Every round in a mag past Minimum Mag Count increases FTF failure percent chance this much. Secondary failure multiplier.");
+			minRoundCount = Config.Bind("_Multipliers", "Minimum Mag Count", 15, "Max mag round counts above this incurs higher unreliability.");
+
+			//			pistolMult = Config.Bind("_Multipliers", "Pistol Failure Multiplier", 1f, "Pistols are higher than others because they are semi.");
+
 			lightPrimerStrikeFailureRate = Config.Bind("Failures - Ammo", "Light Primer Strike Failure Rate", 0.25f, "Valid numbers are 0-100");
 			HangFireRate = Config.Bind("Failures - Ammo", "Hang Fire Rate", 0.1f, "Valid numbers are 0-100");
 
@@ -59,10 +70,9 @@ namespace Meatyceiver2
 			failureToLockSlide = Config.Bind("Failures - Broken Firearm", "Failure to Lock Slide Rate", 0.3f, "Valid numbers are 0-100");
 			SlamfireRate = Config.Bind("Failures - Broken Firearm", "Slam Fire Rate", 0.05f, "Valid numbers are 0-100");
 
-			Harmony.CreateAndPatchAll(typeof(Meatyceiver2));
+			//			var harmoney = new Harmony(Info.Metadata.GUID);
+			Harmony.CreateAndPatchAll(typeof(Meatyceiver));
 			rnd = new System.Random();
-
-			UnityEngine.Debug.Log("Meatyceiver2 loaded!");
 		}
 
 		[HarmonyPatch(typeof(FVRFireArmChamber), "Fire")]
@@ -99,19 +109,22 @@ namespace Meatyceiver2
 			return false;
 		}
 
-		//BEGIN FIREARM FAILURES
-
-
 		[HarmonyPatch(typeof(ClosedBoltWeapon), "BeginChamberingRound")]
 		[HarmonyPatch(typeof(OpenBoltReceiver), "BeginChamberingRound")]
 		[HarmonyPatch(typeof(Handgun), "ExtractRound")]
 		[HarmonyPrefix]
-		static bool FTFPatch()
+		static bool FTFPatch(FVRFireArm __instance)
 		{
+			float failureinc = 0;
 			if (!enableFirearmFailures.Value) { return true; }
 			var rand = (float)rnd.Next(0, 10001) / 100;
 			if (enableConsoleDebugging.Value) { Debug.Log("Random number generated for FTF: " + rand); };
-			if (rand <= failureToFeedRate.Value * generalMult.Value)
+
+			if (__instance.Magazine != null && enableSecondaryMultipliers.Value)
+			{
+				failureinc = (float)(__instance.Magazine.m_capacity - minRoundCount.Value) * failureIncPerRound.Value;
+			}
+			if (rand <= (failureToFeedRate.Value + failureinc) * generalMult.Value)
 			{
 				if (enableConsoleDebugging.Value) { Debug.Log("Failure to feed!"); };
 				return false;
@@ -201,26 +214,26 @@ namespace Meatyceiver2
 					}
 					return true;
 				}*/
-
-
-	//BEGIN BROKEN FIREARM FAILURES
-
-
-
-/*		[HarmonyPatch(typeof(ClosedBoltWeapon), "CockHammer")]
+		[HarmonyPatch(typeof(HandgunSlide), "UpdateSlide")]
 		[HarmonyPrefix]
-		static bool FailureToLockBackPatch()
+		static bool FailureToLockBackPatch(HandgunSlide __instance)
 		{
 			if (!enableBrokenFirearmFailures.Value) { return true; }
-			var rand = (float)rnd.Next(0, 10001) / 100;
-			if (enableConsoleDebugging.Value) { Debug.Log("Random number generated for Hammer Follow: " + rand); };
-			if (rand <= HammerFollowRate.Value * generalMult.Value)
+			if (__instance.CurPos == HandgunSlide.SlidePos.Forward && __instance.LastPos == HandgunSlide.SlidePos.Forward)
 			{
-				if (enableConsoleDebugging.Value) { Debug.Log("Hammer follow!"); };
-				return false;
+				var rand = (float)rnd.Next(0, 10001) / 100;
+				if (enableConsoleDebugging.Value) { Debug.Log("Random number generated for Slam Fire: " + rand); };
+				if (rand <= SlamfireRate.Value * generalMult.Value)
+				{
+					if (enableConsoleDebugging.Value) { Debug.Log("Slam fire!"); };
+					__instance.Handgun.DropHammer(false);
+					return true;
+				}
 			}
 			return true;
-		}*/
+		}
+
+
 
 		[HarmonyPatch(typeof(ClosedBoltWeapon), "CockHammer")]
 		[HarmonyPrefix]
@@ -251,6 +264,5 @@ namespace Meatyceiver2
 			}
 			return true;
 		}
-
 	}
 }
