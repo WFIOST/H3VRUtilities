f6a8cadcf81519a07af91e3cd61b4cf77ebed786
Potatoes <cd64@mail.com>
2021-02-18 22:04:11 -0500

    Create laser.cs

---
+++ pcgH3VRframework/MonoScripts/VisualModifiers/laser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace H3VRUtils.MonoScripts.VisualModifiers
+{
+	class laser : MonoBehaviour
+	{
+		public float maxrange = 1000;
+
+		private LineRenderer lr;
+		void Start()
+		{
+			lr = GetComponent<LineRenderer>();
+		}
+
+		void Update()
+		{
+			lr.SetPosition(0, transform.position);
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.forward, out hit))
+			{
+				if (hit.collider)
+				{
+					lr.SetPosition(1, hit.point);
+				}
+			}
+			else lr.SetPosition(1, transform.forward * maxrange);
+		}
+	}
+}
