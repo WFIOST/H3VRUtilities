aee3d7ef904f4413f3058c32dd632469f7255a4c
Potatoes <cd64@mail.com>
2021-03-01 21:09:22 -0500

    605

--- H3VRUtilities/MonoScripts/UIModifiers/DispBulletAmount.cs
+++ H3VRUtilities/MonoScripts/UIModifiers/DispBulletAmount.cs
@@ -8,23 +8,90 @@ using FistVR;
 
 namespace H3VRUtils.MonoScripts.UIModifiers
 {
-    public class DispBulletAmount : MonoBehaviour
-    {
-        public FVRFireArm firearm;
-        public Text UItext;
-        public string textWhenEmpty;
-        [Tooltip("When there is no mag, the text will remain whatever it was before.")]
-        public bool KeepLastRoundInfoOnEmpty;
-        public void FixedUpdate()
-        {
-            if (firearm.Magazine != null)
-            {
-                UItext.text = firearm.Magazine.m_numRounds.ToString();
-            }
-            else
-            {
-                UItext.text = textWhenEmpty;
-            }
-        }
-    }
+	public class DispBulletAmount : MonoBehaviour
+	{
+		public FVRFireArm firearm;
+		public FVRFireArmMagazine magazine;
+		public Text UItext;
+		public string textWhenNoMag;
+		[Tooltip("When there is no mag, the text will remain whatever it was before.")]
+		public bool KeepLastRoundInfoOnNoMag;
+		public bool AddMinCharLength;
+		public bool IncludeChamberRound;
+		public int MinCharLength;
+		private bool WasFull;
+		private FVRFireArmChamber chamber;
+
+		public void Start()
+		{
+			if (firearm is ClosedBoltWeapon)
+			{
+				var wep = firearm as ClosedBoltWeapon;
+				chamber = wep.Chamber;
+			}
+			if (firearm is OpenBoltReceiver)
+			{
+				var wep = firearm as OpenBoltReceiver;
+				chamber = wep.Chamber;
+			}
+			if (firearm is Handgun)
+			{
+				var wep = firearm as Handgun;
+				chamber = wep.Chamber;
+			}
+		}
+
+		public void FixedUpdate()
+		{
+			string txt = "";
+
+			if (firearm == null)
+			{
+				txt = magazine.m_numRounds.ToString();
+				if (AddMinCharLength)
+				{
+					int lengthneedtoadd = MinCharLength - txt.Length;
+					for (int i = 0; i < lengthneedtoadd; i++) txt = "0" + txt;
+				}
+
+				UItext.text = txt;
+			}
+			else
+			if (firearm.Magazine != null)
+			{
+				int amt = firearm.Magazine.m_numRounds;
+
+				if (IncludeChamberRound)
+				{
+					if (WasFull && !chamber.IsFull)
+					{
+						
+					}
+					else
+					{
+						WasFull = chamber.IsFull;
+						return;
+					}
+
+					WasFull = chamber.IsFull;
+				}
+
+				txt = firearm.Magazine.m_numRounds.ToString();
+				if (AddMinCharLength)
+				{
+					int lengthneedtoadd = MinCharLength - txt.Length;
+					for (int i = 0; i < lengthneedtoadd; i++) txt = "0" + txt;
+				}
+
+				UItext.text = txt;
+			}
+			else
+			{
+				if (!KeepLastRoundInfoOnNoMag)
+				{
+					UItext.text = textWhenNoMag;
+				}
+			}
+		}
+	}
 }
---
+++ H3VRUtilities/MonoScripts/VisualModifiers/InvisibleMagOnLoad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FistVR;
+using UnityEngine;
+
+namespace H3VRUtils
+{
+	class InvisibleMagOnLoad : MonoBehaviour
+	{
+		public FVRFireArmMagazine magazine;
+
+		public void Update()
+		{
+			if(magazine.FireArm != null)
+			{
+				magazine.Viz.gameObject.SetActive(false);
+			}
+			else
+			{
+				magazine.Viz.gameObject.SetActive(true);
+			}
+		}
+	}
+}
