0e97cf14d6e8bc6f82e02b7527865986467d5432
Potatoes <cd64@mail.com>
2021-03-12 22:18:22 -0500

    push

---
+++ H3VRUtilities/Vehicles/General/SteeringWheel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using FistVR;
+
+namespace H3VRUtils.Vehicles
+{
+	public class Lever : FVRInteractiveObject
+	{
+		protected override void Awake()
+		{
+			base.Awake();
+		}
+
+		public override void UpdateInteraction(FVRViveHand hand)
+		{
+			base.UpdateInteraction(hand);
+			Vector3 vector = hand.transform.position - this.LeverTransform.position;
+			Vector3 to = Vector3.ProjectOnPlane(vector, this.LeverTransform.right);
+			if (Vector3.Dot(to.normalized, this.Base.up) > 0f)
+			{
+				this.m_curRot = -Vector3.Angle(this.Base.forward, to);
+			}
+			else
+			{
+				this.m_curRot = Vector3.Angle(this.Base.forward, to);
+			}
+		}
+
+		public float GetLeverValue()
+		{
+			return Mathf.InverseLerp(this.minValue, this.maxValue, this.m_curRot);
+		}
+
+		protected override void FVRUpdate()
+		{
+			base.FVRUpdate();
+			this.m_curRot = Mathf.Clamp(this.m_curRot, minValue, maxValue);
+			this.LeverTransform.localEulerAngles = new Vector3(this.m_curRot, 0f, 0f);
+		}
+
+		public Transform LeverTransform;
+		public Transform Base;
+		public float m_curRot = -22.5f;
+		public float minValue = -22.5f;
+		public float maxValue = 22.5f;
+
+		public enum LeverState
+		{
+			Off,
+			Mid,
+			On
+		}
+	}
+}
---
+++ H3VRUtilities/Vehicles/General/VehicleMovementHandling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using FistVR;
+
+namespace H3VRUtils.Vehicles
+{
+	class VehicleMovementHandling : MonoBehaviour
+	{
+		public GameObject vehicle;
+		private Rigidbody rb;
+		public float force;
+		public FVRViveHand hand;
+		public Transform SitPos;
+
+		void Start()
+		{
+			rb = GetComponent<Rigidbody>();
+		}
+
+		public void FixedUpdate()
+		{
+			if (hand.A_Button.active)
+			{
+				ApplyThrust();
+			}
+		}
+
+		public void Update()
+		{
+			
+		}
+
+
+		public void ApplyThrust()
+		{
+			rb.AddForce(transform.forward * force);
+		}
+	}
+}
