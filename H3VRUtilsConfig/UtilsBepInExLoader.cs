64fa1a7d45c3b838f870ccec89a4a2a34d5eeba5
Potatoes <cd64@mail.com>
2021-06-19 12:05:17 -0400

    set force mag drop off by default, csproj updated, added OpenBoltBurstFire

--- H3VRUtilsConfig/UtilsBepInExLoader.cs
+++ H3VRUtilsConfig/UtilsBepInExLoader.cs
@@ -36,7 +36,7 @@ namespace H3VRUtils
 		void Start()
 		{
 			paddleMagRelease = Config.Bind("General Settings", "Enable Paddle Release", false, "Allows custom guns to utilize the feature to require a direction press on the touchpad to release the mag, usually to simulate a paddle release.");
-			magDropRequiredRelease = Config.Bind("General Settings", "Enable Mandatory Mag Drop", true, "Allows custom guns to utilize the feature to require the mag to be dropped by your primary hand, even if your other hand is gripping the magazine.");
+			magDropRequiredRelease = Config.Bind("General Settings", "Enable Mandatory Mag Drop", false, "Allows custom guns to utilize the feature to require the mag to be dropped by your primary hand, even if your other hand is gripping the magazine.");
 
 			paddleMagReleaseDir = Config.Bind("Fine Tuning", "Enhanced Mag Release Direction", TouchpadDirTypePT.BasedOnWeapon, "Based On Weapon is the default direction chosen by the modmaker. Others are overrides.");
 
