4f16656968e0d5110dfd8c21be9f2639e9daebe8
Potatoes <cd64@mail.com>
2021-07-21 11:57:50 -0400

    test file

---
+++ H3VRUtilities/src/FVRInteractiveObjects/lockToHead.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FistVR;
+using UnityEngine;
+
+namespace H3VRUtils
+{
+	class lockToHead : MonoBehaviour
+	{
+		public Vector3 posOffset;
+		public void Update()
+		{
+			this.transform.position = GM.CurrentPlayerBody.Head.transform.position;
+			transform.localPosition += posOffset;
+			this.transform.rotation = GM.CurrentPlayerBody.Head.transform.rotation;
+		}
+	}
+}
