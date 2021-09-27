b991ddeb88aa816c5c36c7f0fde132cd543b6039
Potatoes <cd64@mail.com>
2021-09-27 16:41:01 -0400

    MultipleChargingHandleClosedBolt.cs added

---
+++ H3VRUtilities/src/FVRInteractiveObjects/MultipleChargingHandleClosedBolt.cs
@@ -0,0 +1,19 @@
+﻿using FistVR;
+using UnityEngine;
+
+namespace H3VRUtils.FVRInteractiveObjects
+{
+	public class MultipleChargingHandleClosedBolt : ClosedBoltHandle
+	{
+		public override void BeginInteraction(FVRViveHand hand)
+		{
+			if (Weapon.Handle.m_hand != null)
+			{
+				base.ForceBreakInteraction();
+				return;
+			}
+			Weapon.Handle = this;
+			base.BeginInteraction(hand);
+		}
+	}
+}
