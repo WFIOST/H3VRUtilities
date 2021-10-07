f81209b28c3cb39f2291a14a31a17accb4ea024b
Potatoes <cd64@mail.com>
2021-10-07 19:18:01 -0400

    Updated BepInEx version; Updated Sodalite version

--- H3VRUtilities/src/AndrewContributions/ThermiteFire.cs
+++ H3VRUtilities/src/AndrewContributions/ThermiteFire.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using UnityEngine;
 using FistVR;
+using UnityEngine;
 using Unity;
 
 namespace Plugin.src
--- H3VRUtilities/src/FVRInteractiveObjects/attachmentCode/AttachmentModifyFirearm.cs
+++ H3VRUtilities/src/FVRInteractiveObjects/attachmentCode/AttachmentModifyFirearm.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using FistVR;
-using HarmonyLib;
 
 namespace H3VRUtils
 {
