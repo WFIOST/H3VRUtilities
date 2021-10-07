de8d73eec0ce1dce82c07a1b9ce3cf179546acf2
Potatoes <cd64@mail.com>
2021-10-07 18:51:16 -0400

    RotateTowardsPlayer.cs and SouthpawSupporter.cs added

---
+++ H3VRUtilities/src/QOL/RotateTowardsPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FistVR;
+using UnityEngine;
+
+namespace H3VRUtils.MonoScripts.VisualModifiers
+{
+	public class RotateTowardsPlayer : MonoBehaviour
+	{
+		public bool rotateOnX;
+		public bool rotateOnY;
+		public bool rotateOnZ;
+		public void Start()
+		{
+			var rot = gameObject.transform.localEulerAngles;
+			gameObject.transform.LookAt(GM.CurrentPlayerRoot);
+			if (rotateOnX) rot.x = gameObject.transform.localEulerAngles.x;
+			if (rotateOnY) rot.y = gameObject.transform.localEulerAngles.y;
+			if (rotateOnZ) rot.z = gameObject.transform.localEulerAngles.z;
+			gameObject.transform.localEulerAngles = rot;
+		}
+	}
+}
---
+++ H3VRUtilities/src/QOL/SouthpawSupporter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using FistVR;
+using UnityEngine;
+
+namespace H3VRUtils.QOL
+{
+	public class SouthpawSupporter : MonoBehaviour
+	{
+		//i don't believe southpaws deserve rights but apparently i've been asked to make this too many times
+		[Tooltip("If true, automatically generates LeftHand and LeftHand_Touch.m")]
+		public bool AutoSwapRot = true;
+		public Transform LeftHand;
+		public Transform LeftHand_Touch;
+		private Transform RightHand;
+		private Transform RightHand_Touch;
+		private FVRPhysicalObject _physicalObject;
+
+		public void Start()
+		{
+			_physicalObject = GetComponent<FVRPhysicalObject>();
+			RightHand = _physicalObject.PoseOverride;
+			RightHand_Touch = _physicalObject.PoseOverride_Touch;
+			if (AutoSwapRot)
+			{
+				LeftHand = setupEmpty(_physicalObject.PoseOverride).transform;
+				LeftHand_Touch = setupEmpty(_physicalObject.PoseOverride_Touch).transform;
+				var pos = LeftHand.position;
+				var rot = LeftHand.localEulerAngles;
+				pos.x = -pos.x;
+				LeftHand.localPosition = pos;
+				rot.y = -rot.y; rot.z = -rot.z;
+				LeftHand.localEulerAngles = rot;
+				
+				pos = LeftHand_Touch.position;
+				rot = LeftHand_Touch.localEulerAngles;
+				pos.x = -pos.x;
+				LeftHand_Touch.localPosition = pos;
+				rot.y = -rot.y; rot.z = -rot.z;
+				LeftHand_Touch.localEulerAngles = rot;
+			}
+		}
+
+		public static GameObject setupEmpty(Transform tf)
+		{
+			var go = new GameObject();
+			go.transform.parent = tf;
+			go.transform.localPosition = Vector3.zero;
+			go.transform.localEulerAngles = Vector3.zero;
+			go.transform.localScale = Vector3.zero;
+			go.transform.parent = tf.parent;
+			return go;
+		}
+		
+		public void Update()
+		{
+			if (_physicalObject.IsHeld)
+			{
+				if (_physicalObject.m_hand != null)
+				{
+					if (!_physicalObject.m_hand.IsThisTheRightHand)
+					{
+						_physicalObject.PoseOverride = LeftHand;
+						_physicalObject.PoseOverride_Touch = LeftHand_Touch;
+					}
+					else
+					{
+						_physicalObject.PoseOverride = RightHand;
+						_physicalObject.PoseOverride = RightHand_Touch;
+					}
+				}
+			}
+		}
+	}
+}
