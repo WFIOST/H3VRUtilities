21e497908aea7671435819737972dc2458ebd9b6
Potatoes <cd64@mail.com>
2021-10-16 13:50:37 -0400

    Refactored DispBulletAmount.cs -SetEnabledObjects now can lerp -MaxAmmo now can lerp -Fixed some incorrect code -SetEnabledObjects now clamp amt of ammo to object count

--- H3VRUtilities/src/Visuals/DispBulletAmount.cs
+++ H3VRUtilities/src/Visuals/DispBulletAmount.cs
@@ -13,18 +13,29 @@ namespace H3VRUtils.MonoScripts.UIModifiers
 	//thanks frityet for the rewrite!
 	public class DispBulletAmount : MonoBehaviour
 	{
+		[Header("Only fill out one of these.")]
 		public FVRFireArm firearm;
 		public FVRFireArmMagazine magazine;
 		public FVRFireArmAttachment attachment;
 
 		[Header("Ammo Counter Settings")]
+		[Tooltip("Text to display ammo left in the gun.")]
 		public Text UItext;
+		[Tooltip("Text to display maximum ammo.")]
 		public Text MaxAmmoText;
+		[Tooltip("Text to display the type of ammo.")]
 		public Text ammoTypeText;
+		[Tooltip("Adds a minimum character count. See tooltip for MinCharLength for more.")]
 		public bool AddMinCharLength;
+		[Tooltip("i.e if MinCharLength is 2 and the amount of rounds left is 5, it will display 05 instead of 5.")]
 		public int MinCharLength;
-
+		
+		[Tooltip("Will not instantly be the correct amount of rounds, but will tick up/down until it is.")]
 		public bool enableDispLerp;
+		[Tooltip("MaxAmmo will also lerp according to the DispLerpAmt")]
+		public bool enableLerpForMaxAmmo;
+		[Tooltip("EnabledObjects will also lerp according to the DispLerpAmt")]
+		public bool enableLerpForEnabledObjects;
 		[Tooltip("From 0-1. The % amount moved towards its correct amount every 50th of a second.")]
 		[Range(0f, 1f)]
 		public float DispLerpAmt;
@@ -36,12 +47,14 @@ namespace H3VRUtils.MonoScripts.UIModifiers
 		public GameObject ObjectWhenEmpty;
 		[Tooltip("Element no. corresponds to rounds left. 0 means no rounds, 1 means one round. Enables the 5th object if there are 5 rounds, and so on.")]
 		public List<GameObject> Objects;
-		[Tooltip("Enables all objects under the round count. Enables the 0nd, 1st, 2nd, 3rd objects if there are 3 rounds left, and so on.")]
+		[Tooltip("Enables all objects under the round count. Enables the 0th, 1st, 2nd, 3rd objects if there are 3 rounds left, and so on.")]
 		public bool EnableAllUnderAmount;
 		
 		private FVRFireArm _fa;
 		private FVRFireArmMagazine _mag;
-		private int bulletamts;
+		private int lastAmountOfBullets;
+		private int lastAmountOfMaxBullets;
+		private int lastAmountOfBulletsForEnableObjects; //this is now a game to get the longest var name
 
 		private void GetFirearmAndMag()
 		{
@@ -100,40 +113,72 @@ namespace H3VRUtils.MonoScripts.UIModifiers
 			if (_fa != null) {
 				return AM.GetFullRoundName(_fa.RoundType, _fa.GetChamberRoundList()[0]);
 			}
-
 			return String.Empty;
 		}
 
 		private void Update()
 		{
-			GetFirearmAndMag();
-			var amtAmmo = GetAmmoCount();
-			bool COIoverride = amtAmmo == 0;
-			if (enableDispLerp)
+			GetFirearmAndMag(); //update firearm and mag if needed
+			
+			if (_isUItextNotNull) //set ammo text
 			{
-				amtAmmo = Mathf.CeilToInt(Mathf.Lerp(bulletamts, amtAmmo, DispLerpAmt));
+				string amountOfAmmoString = CalculateAmmoCounterAmount(GetAmmoCount(), ref lastAmountOfBullets,
+					enableDispLerp, DispLerpAmt, AddMinCharLength, MinCharLength);
+				UItext.text = amountOfAmmoString; //set uitext
 			}
-
-			if (amtAmmo == 1 && COIoverride) amtAmmo = 0;
-			
-			
-			bulletamts = amtAmmo;
-			string amtAmmoString = amtAmmo.ToString();
-			if (AddMinCharLength) { //most certainly a faster way but idc
-				int lengthneedtoadd = MinCharLength - amtAmmoString.Length;
-				for (int i = 0; i < lengthneedtoadd; i++) amtAmmoString = "0" + amtAmmoString;
+			if (_isMaxAmmoTextNotNull) //set max ammo text
+			{
+				string amountOfAmmoString = CalculateAmmoCounterAmount(GetAmmoCount(), ref lastAmountOfMaxBullets,
+					enableLerpForMaxAmmo, DispLerpAmt, AddMinCharLength, MinCharLength);
+				MaxAmmoText.text = amountOfAmmoString; //set maxammo
 			}
-			if(UItext != null)
-				UItext.text = amtAmmo.ToString();
-			if(MaxAmmoText != null)
-				MaxAmmoText.text = GetMaxAmmoCount().ToString();
-			if(ammoTypeText != null)
-				ammoTypeText.text = GetAmmoType();
-			if(EnabledObjects) SetEnabledObjects(amtAmmo);
+			if(_isammoTypeTextNotNull) ammoTypeText.text = GetAmmoType();
+			if (EnabledObjects)
+			{
+				int amountOfAmmo = int.Parse(CalculateAmmoCounterAmount(GetAmmoCount(), ref lastAmountOfBulletsForEnableObjects,
+					enableLerpForEnabledObjects, DispLerpAmt));
+				SetEnabledObjects(amountOfAmmo); //set uitext
+			}
 		}
+
+		#region Function Shortcuts
+
+		public static string CalculateAmmoCounterAmount(
+			int currentammo, ref int lastammo,
+			bool lerp = false, float lerpAmt = 0, bool pad = false, int padAmt = 0)
+		{
+			//lerp ammo amount
+			if(lerp) currentammo = LerpInt(lastammo, currentammo, lerpAmt);
+			//make string of amtofammo, pad it
+			string amountOfAmmoString = currentammo.ToString();
+			if (pad) amountOfAmmoString = PadStringNumberToAmt(amountOfAmmoString, padAmt);
+			lastammo = currentammo;
+			return amountOfAmmoString;
+		}
+		
+		public static string PadStringNumberToAmt(string str, int minCharLength)
+		{
+			//most certainly a faster way but idc
+			int lengthneedtoadd = minCharLength - str.Length; //calc 0s needed to add for string
+			for (int i = 0; i < lengthneedtoadd; i++) str = "0" + str;
+			return str;
+		}
+
+		public static int LerpInt(int a, int b, float lerp)
+		{
+			//CeilToIntOverride is here because otherwise the lerp would never result in a 0
+			//(only if lerp was 1 would CeilToInt return 0 normally)
+			bool COIoverride = a == 0;
+			a = Mathf.CeilToInt(Mathf.Lerp(a, b, lerp));
+			if (a == 1 && COIoverride) a = 0;
+			return a;
+		}
+		#endregion
 
 		private void SetEnabledObjects(int amt)
 		{ //yoinked from old bit. TODO: rewrite this plz
+			
+			amt = Mathf.Clamp(amt, 0, Objects.Count - 1);
 			
 			for (int i = 0; i < Objects.Count; i++) //set all to false
 			{
@@ -162,23 +207,41 @@ namespace H3VRUtils.MonoScripts.UIModifiers
 				}
 		}
 
+		#region Start stuff
+		private void Start()
+		{
+			NullCheck();
+			SafetyCheck();
+		}
+		
 		private bool _isFireArmNotNull;
 		private bool _isMagazineNotNull;
 		private bool _isAttachmentNotNull;
-		private void Start()
+		private bool _isUItextNotNull;
+		private bool _isMaxAmmoTextNotNull;
+		private bool _isammoTypeTextNotNull;
+
+		public void NullCheck() //if for some reason some foreign script wants to modify a null param just call this
 		{
+			_isammoTypeTextNotNull = ammoTypeText != null;
+			_isMaxAmmoTextNotNull = MaxAmmoText != null;
+			_isUItextNotNull = UItext != null;
 			_isFireArmNotNull = firearm != null;
 			_isMagazineNotNull = magazine != null;
 			_isAttachmentNotNull = attachment != null;
-			
-			//cast all three to int, add
+		}
+
+		private void SafetyCheck()
+		{
+			//cast all three to int, add. also this is dumb lol
 			int i   = (_isFireArmNotNull ? 1 : 0) 
-			        + (_isMagazineNotNull ? 1 : 0)
-					+ (_isAttachmentNotNull ? 1 : 0);
-			if (i > 2)
+			          + (_isMagazineNotNull ? 1 : 0)
+			          + (_isAttachmentNotNull ? 1 : 0);
+			if (i > 1)
 				Debug.LogWarning("AmmoCounter has more than one field filled out! Is this supposed to be on a firearm, magazine, or attachment? Choose one!");
 			else if (i == 0)
 				Debug.LogWarning("AmmoCounter doesn't have any field filled out!");
 		}
+		#endregion
 	}
 }
