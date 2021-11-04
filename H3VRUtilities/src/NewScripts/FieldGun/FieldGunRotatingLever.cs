0629b1ebe00d76d1de1ff64c70ae80c0860f7f96
Potatoes <cd64@mail.com>
2021-11-04 15:59:01 -0400

    Field guns updated

---
+++ H3VRUtilities/src/NewScripts/FieldGun/FieldGunRotatingLever.cs
@@ -0,0 +1,12 @@
+﻿using H3VRUtils.FVRInteractiveObjects;
+using UnityEngine;
+
+namespace H3VRUtils.NewScripts
+{
+	public class FieldGunRotatingLever : RotatingObject
+	{
+		public FieldGun fieldGun;
+		public enum axis { x,y,z }
+		public axis rotationDir;
+	}
+}
