003ed8d668bd96a64d090f6bb2bde90f3dd4fdac
Potatoes <cd64@mail.com>
2021-11-20 21:22:36 -0500

    DumpInternalMag.cs for handguns added

---
+++ H3VRUtilities/src/UniqueCode/DumpInternalMag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FistVR;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace H3VRUtils.UniqueCode
+{
+	public class DumpInternalMag : MonoBehaviour
+	{
+		public Handgun handgun;
+		public H3VRUtilsMagRelease.TouchpadDirType presstoejectbutton;
+
+		public void FixedUpdate()
+		{
+			if (!handgun.IsHeld) return;
+			var hand = handgun.m_hand;
+			var dir = H3VRUtilsMagRelease.TouchpadDirTypeToVector2(presstoejectbutton);
+			if (Vector2.Angle(hand.Input.TouchpadAxes, dir) <= 45f
+			    && hand.Input.TouchpadDown
+			    && hand.Input.TouchpadAxes.magnitude > 0.2f)
+			{
+				if (handgun.Magazine.HasARound() && !handgun.Chamber.IsFull && !handgun.m_proxy.IsFull)
+				{
+					var go = handgun.Magazine.RemoveRound(false);
+					handgun.m_proxy.SetFromPrefabReference(go);
+					handgun.Chamber.SetRound(handgun.m_proxy.Round);
+					handgun.EjectExtractedRound();
+				}
+			}
+		}
+	}
+}
