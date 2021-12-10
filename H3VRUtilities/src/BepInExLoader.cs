2cd9e4b9d61541c90750855528d248723b8436d0
Potatoes <cd64@mail.com>
2021-12-09 19:28:19 -0500

    the name's genuine genuinely stupid also push to v8.8.3

---
+++ H3VRUtilities/src/BepInExLoader.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace H3VRUtils
+{
+	[BepInPlugin("dll.wfiost.h3vrutilitieslib", "H3VR Utilities Library", "8.8.3")]
+	[BepInDependency("dll.wfiost.h3vrutilities", BepInDependency.DependencyFlags.HardDependency)]
+	[BepInProcess("h3vr.exe")]
+	public class BepInExLoader : BaseUnityPlugin
+	{
+		
+	}
+}
