96caad3a379b8e715fcc05e7e044ab7b4d5dc595
Potatoes <cd64@mail.com>
2021-12-24 20:47:39 -0500

    bump to ver 8.9.0

--- H3VRUtilsConfig/src/UtilsBepInExLoader.cs
+++ H3VRUtilsConfig/src/UtilsBepInExLoader.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 
 namespace H3VRUtils
 {
-	[BepInPlugin("dll.wfiost.h3vrutilities", "H3VR Utilities", "8.8.3")]
+	[BepInPlugin("dll.wfiost.h3vrutilities", "H3VR Utilities", "8.9.0")]
 	[BepInDependency("nrgill28.Sodalite", BepInDependency.DependencyFlags.SoftDependency)]
 	[BepInProcess("h3vr.exe")]
 	public class UtilsBepInExLoader : BaseUnityPlugin
