59df2e7d141799898331261b74968e6b97268205
Potatoes <cd64@mail.com>
2022-03-14 21:01:52 -0400

    Push to v8.9.2

--- H3VRUtilities/src/BepInExLoader.cs
+++ H3VRUtilities/src/BepInExLoader.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace H3VRUtils
 {
-	[BepInPlugin("dll.wfiost.h3vrutilitieslib", "H3VR Utilities Library", "8.9.1")]
+	[BepInPlugin("dll.wfiost.h3vrutilitieslib", "H3VR Utilities Library", "8.9.2")]
 	[BepInDependency("dll.wfiost.h3vrutilities", BepInDependency.DependencyFlags.HardDependency)]
 	[BepInProcess("h3vr.exe")]
 	public class BepInExLoader : BaseUnityPlugin
