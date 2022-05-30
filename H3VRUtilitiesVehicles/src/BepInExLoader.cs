b805c87663c3f6433bb702c0a901036dd2cfa3c7
Potatoes <cd64@mail.com>
2022-05-30 16:10:36 -0400

    Push update for vehicles lib to be loaded thru BepInEx

--- H3VRUtilitiesVehicles/src/BepInExLoader.cs
+++ H3VRUtilitiesVehicles/src/BepInExLoader.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 namespace H3VRUtils
 {
-	[BepInPlugin("dll.wfiost.h3vrutilitieslib", "H3VR Utilities Library", "8.9.2")]
+	[BepInPlugin("dll.wfiost.h3vrutilitieslib.vehicles", "H3VR Utilities Vehicles Library", "8.9.2")]
 	[BepInDependency("dll.wfiost.h3vrutilities", BepInDependency.DependencyFlags.HardDependency)]
 	[BepInProcess("h3vr.exe")]
-	public class BepInExLoader_Lib : BaseUnityPlugin
+	public class BepInExLoader_VehiclesLib : BaseUnityPlugin
 	{
 		
 	}
